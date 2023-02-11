9b50e2f0cdbff3128b6aadcb7b8e75bf88d07a07
David Alfonso <davidag@users.noreply.github.com>
2023-02-11 20:31:36 +0100

    week 3: homework setup

---
+++ week_3_data_warehouse/blocks/make_gcp_blocks.py
@@ -0,0 +1,20 @@
+from prefect_gcp import GcpCredentials
+from prefect_gcp.cloud_storage import GcsBucket
+
+
+# Prefect constants
+CREDENTIALS_BLOCK_NAME = "zoom-gcp-creds"  # Assumes this one already exists from last week
+BUCKET_BLOCK_NAME = "zoom-gcs-week3"
+
+# Google Cloud constants
+BUCKET_GCP_NAME = "data-warehouse-week3_dtc-de-375612"
+
+# Create or update Google Cloud Storage block in Prefect
+bucket_block = GcsBucket(
+    gcp_credentials=GcpCredentials.load(CREDENTIALS_BLOCK_NAME),
+    bucket=BUCKET_GCP_NAME,
+)
+
+doc_id = bucket_block.save(BUCKET_BLOCK_NAME, overwrite=True)
+
+print(f"GcsBucket created: {doc_id}")
---
+++ week_3_data_warehouse/flows/etl_web_to_gcs.py
@@ -0,0 +1,44 @@
+import argparse
+from pathlib import Path
+
+import requests
+
+from prefect import flow, task
+from prefect_gcp.cloud_storage import GcsBucket
+
+
+BUCKET_BLOCK_NAME = "zoom-gcs-week3"
+
+
+@task()
+def write_gcs_from_url(dataset_url: str, dataset_file: str) -> None:
+    """Upload local parquet file to GCS"""
+    gcs_block = GcsBucket.load(BUCKET_BLOCK_NAME)
+    r = requests.get(dataset_url, stream=True)
+    gcs_block.upload_from_file_object(r.raw, dataset_file)
+
+
+@flow()
+def etl_web_to_gcs(color: str, year: int, months: list[int]) -> None:
+    """The main ETL function"""
+    for month in months:
+        print(f"Processing month {month:02}...")
+        dataset_file = f"{color}_tripdata_{year}-{month:02}.csv.gz"
+        dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}"
+        write_gcs_from_url(dataset_url, dataset_file)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Load Taxi data into Google Cloud Storage')
+
+    parser.add_argument('--color', type=str, required=True, help='Taxi color data')
+    parser.add_argument('--year', type=int, required=True, help='Data year')
+    parser.add_argument('--months', type=str, required=False, help='Comma-separated list of months. All months if ommitted.')
+    args = parser.parse_args()
+
+    if args.months:
+        months = [int(m) for m in args.months.split(",")]
+    else:
+        months = [i for i in range(1, 12 + 1)]
+
+    etl_web_to_gcs(args.color, args.year, months)
